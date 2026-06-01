e06a635ff4413b9d07423ce11dc30784f49f7440
Khalil DIDA <khalildida93@gmail.com>
2026-06-01 13:17:30 +0100

    updates

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,13 @@ fn main(){
         let mut guess: String = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read number");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                print!("Not a valid number");
+                continue;
+            },
+        };
 
 
         println!("==> {}", guess);
